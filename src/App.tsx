1019ac4245e19f5115073d6475a25585213c16c7
alina <kostenkoalina97@icloud.com>
2025-08-20 16:46:35 +0200

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
 
-// import { getAll, get5First, getRed } from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { Good } from './types/Good';
 // or
 // import * as goodsAPI from './api/goods';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+export const App: React.FC = () => {
+  const [visibleGoods, setVisibleGoods] = useState<Good[]>([]);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  const handleGetAllGoods = () => {
+    setErrorMessage(null);
+    getAll().then(setVisibleGoods).catch(setErrorMessage);
+  };
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+  const handleGet5Goods = () => {
+    setErrorMessage(null);
+    get5First().then(setVisibleGoods).catch(setErrorMessage);
+  };
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+  const handleGetRed = () => {
+    setErrorMessage(null);
+    getRedGoods().then(setVisibleGoods).catch(setErrorMessage);
+  };
 
-    <GoodsList goods={[]} />
-  </div>
-);
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
+
+      <button type="button" data-cy="all-button" onClick={handleGetAllGoods}>
+        Load all goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={handleGet5Goods}
+      >
+        Load 5 first goods
+      </button>
+
+      <button type="button" data-cy="red-button" onClick={handleGetRed}>
+        Load red goods
+      </button>
+
+      {errorMessage && <p className="error">{errorMessage}</p>}
+      <GoodsList goods={visibleGoods} />
+    </div>
+  );
+};
