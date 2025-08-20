d16f27bdc311da9bf821196ce73d6b62c01beada
alina <kostenkoalina97@icloud.com>
2025-08-20 17:15:40 +0200

    add react.memo

--- src/GoodsList.tsx
+++ src/GoodsList.tsx
@@ -5,7 +5,7 @@ type Props = {
   goods: Good[];
 };
 
-export const GoodsList: React.FC<Props> = ({ goods }) => (
+const ListOfGoods: React.FC<Props> = ({ goods }) => (
   <ul>
     {goods.map(good => (
       <li key={good.id} style={{ color: good.color }} data-cy="good">
@@ -14,3 +14,5 @@ export const GoodsList: React.FC<Props> = ({ goods }) => (
     ))}
   </ul>
 );
+
+export const GoodsList = React.memo(ListOfGoods);
